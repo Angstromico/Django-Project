5013539da87ea631211e70e98f13a1ed70ed289d
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-04 10:52:11 -0400

    feat: setting reservation model

--- project/firstapp/models.py
+++ project/firstapp/models.py
@@ -8,3 +8,15 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    date = models.DateField(auto_now=True)
+    time = models.TimeField()
+    guests = models.IntegerField()
+    comments = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.date} {self.time}"
